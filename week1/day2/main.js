7a0b06c022b63c8fb7d9295af68731eda8d695c0
fcurtsson <fanny@curtsson.com>
2022-12-02 12:58:03 +0100

    day 1, part 2

--- week1/day2/main.js
+++ week1/day2/main.js
@@ -21,16 +21,41 @@ const playScoreByIndexDict = {
     'Y': ['C', 'B', 'A'],
     'Z': ['A', 'C', 'B']
 }
+// part 2: outcomes ordered to correspond to playScoreByIndexDict from part 1
+const orderedOutcomes = ['X', 'Y', 'Z']
 
 function getScoreOfRound(p1, p2) {
 
-    const shapeScore = shapeScoreByIndex.findIndex(shape => shape === p2) + 1
-
-    const playScoreByIndex = playScoreByIndexDict[p2]
-    const playScore = playScoreByIndex.findIndex(play => play === p1) * 3
+    const shapeScore = getShapeScore(p2)
+    const playScore = getPlayScore(p1, p2)
 
     return shapeScore + playScore
 }
+
+function getShapeScore(shape) {
+    return shapeScoreByIndex.findIndex(sh => sh === shape) + 1
+}
+
+// p1 = their play, p2 = your play
+function getPlayScore(p1, p2) {
+    const playScoreByIndex = playScoreByIndexDict[p2]
+    return playScoreByIndex.findIndex(play => play === p1) * 3
+}
+
+function getShapeByOutcome(p1, outcome) {
+    const outcomeIndex = orderedOutcomes.findIndex(oc => oc === outcome)
+
+    const shape = Object.keys(playScoreByIndexDict).find(p2 => {
+        const sortedPlays = playScoreByIndexDict[p2]
+
+        if (sortedPlays[outcomeIndex] === p1) {
+            return true
+        }
+        return false
+    })
+    return shape
+}
+
 
 function part1() {
 
@@ -40,13 +65,21 @@ function part1() {
         allRounds += score
         return allRounds
     }, 0)
-    
+
     console.log("Part 1: Total score: ", totalScore)
 }
 
 function part2() {
-    
-    console.log("Part 2: ")
+
+    const totalScore = runData.reduce((allRounds, round) => {
+        const [p1, outcome] = round.split(" ")
+        const p2 = getShapeByOutcome(p1, outcome)
+        const score = getScoreOfRound(p1, p2)
+        allRounds += score
+        return allRounds
+    }, 0)
+
+    console.log("Part 2: Total score: ", totalScore)
 }
 
 part1()
